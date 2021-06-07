c71778544343a4156a508cdf3143e371d6378b8f
3cv50MayteSMproyecto <85509771+3cv50MayteSMproyecto@users.noreply.github.com>
2021-06-07 12:29:23 -0500

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,18 @@
+/*  Inicializa Firebase con la
+ * configuración del proyecto.
+ * Revisa la configuración en tu
+ * servidor de Firebase y cópiala
+ * aquí sustituyendo los
+ * asteriscos. Los campos deben
+ * quedar igual que en tu
+ * servidor. */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyCZ8Y_UoRYIhwi188wA2jAQTOotsWuVjp4",
+    authDomain: "cv50maytesmproyecto.firebaseapp.com",
+    projectId: "cv50maytesmproyecto",
+    storageBucket: "cv50maytesmproyecto.appspot.com",
+    messagingSenderId: "777954176757",
+    appId: "1:777954176757:web:d852bc2f75e61bc8bd9fbc",
+    measurementId: "G-31JR5GRV4Y"
+});
