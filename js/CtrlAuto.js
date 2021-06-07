697a89db1a6b8a716f4f0341ba5dc281bb32f8b9
3cv50MayteSMproyecto <85509771+3cv50MayteSMproyecto@users.noreply.github.com>
2021-06-07 11:42:08 -0500

    Create CtrlAuto.js

---
+++ js/CtrlAuto.js
@@ -0,0 +1,106 @@
+import {
+  getAuth,
+  getFirestore
+} from "../lib/fabrica.js";
+import {
+  getString,
+  muestraError
+} from "../lib/util.js";
+import {
+  muestraAutos
+} from "./navegacion.js";
+import {
+  tieneRol
+} from "./seguridad.js";
+
+const daoAuto =
+  getFirestore().
+    collection("Auto");
+const params =
+  new URL(location.href).
+    searchParams;
+const id = params.get("id");
+/** @type {HTMLFormElement} */
+const forma = document["forma"];
+
+getAuth().onAuthStateChanged(
+  protege, muestraError);
+
+/** @param {import(
+    "../lib/tiposFire.js").User}
+    usuario */
+async function protege(usuario) {
+  if (tieneRol(usuario,
+    ["Administrador"])) {
+    busca();
+  }
+}
+
+/** Busca y muestra los datos que
+ * corresponden al id recibido. */
+async function busca() {
+  try {
+    const doc =
+      await daoAuto.
+        doc(id).
+        get();
+    if (doc.exists) {
+      /**
+       * @type {
+          import("./tipos.js").
+                  Pasatiempo} */
+      const data = doc.data();
+      forma.nombre.value =
+        data.nombre || "";
+      forma.addEventListener(
+        "submit", guarda);
+      forma.eliminar.
+        addEventListener(
+          "click", elimina);
+    } else {
+      throw new Error(
+        "No se encontró.");
+    }
+  } catch (e) {
+    muestraError(e);
+    muestraAutos();
+  }
+}
+
+/** @param {Event} evt */
+async function guarda(evt) {
+  try {
+    evt.preventDefault();
+    const formData =
+      new FormData(forma);
+    const nombre = getString(
+      formData, "nombre").trim();
+    /**
+     * @type {
+        import("./tipos.js").
+                Pasatiempo} */
+    const modelo = {
+      nombre
+    };
+    await daoAuto.
+      doc(id).
+      set(modelo);
+    muestraAutos();
+  } catch (e) {
+    muestraError(e);
+  }
+}
+
+async function elimina() {
+  try {
+    if (confirm("Confirmar la " +
+      "eliminación")) {
+      await daoAuto.
+        doc(id).
+        delete();
+      muestraAutos();
+    }
+  } catch (e) {
+    muestraError(e);
+  }
+}
